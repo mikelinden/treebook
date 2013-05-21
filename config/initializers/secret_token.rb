a356527bfd25b6c113897432af426b8b5e3c35ab
Michael Linden <maicou@gmail.com>
2013-05-21 16:36:31 -0300

    criado primeiro scaffold para update de status

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cnanet::Application.config.secret_token = '0b261e8771e5f455f97a96745e510e141cd6d17a3deb43cd03982d07cec730bba63aa2d5135d2da087d375e7b276a3e4d6e43135a699d183f55aa18c69f5f73a'

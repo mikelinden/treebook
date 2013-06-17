3a199bb2d8eaa814358f5ad7cac6d0f560dad334
Michael Linden <maicou@gmail.com>
2013-06-17 16:58:10 -0300

    add status validation and tests

--- test/functional/statuses_controller_test.rb
+++ test/functional/statuses_controller_test.rb
@@ -11,14 +11,28 @@ class StatusesControllerTest < ActionController::TestCase
     assert_not_nil assigns(:statuses)
   end
 
-  test "should get new" do
+  test "should be redirected when not logged in" do
+    get :new
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end
+
+  test "should render new page when logged in" do
+    sign_in users(:michael)
     get :new
     assert_response :success
   end
+  
+  test "should be logged in to post a status" do
+    post :create, status: { content: "Hello"}
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end
 
-  test "should create status" do
+  test "should create status when logged in" do
+    sign_in users(:michael)
     assert_difference('Status.count') do
-      post :create, status: { content: @status.content, name: @status.name }
+      post :create, status: { content: @status.content}
     end
 
     assert_redirected_to status_path(assigns(:status))
@@ -29,13 +43,27 @@ class StatusesControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should get edit" do
+  test "should be redirected when try to edit and is not logged in" do
+    get :edit, id: @status
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end
+
+  test "should get edit when logged in" do
+    sign_in users(:michael)
     get :edit, id: @status
     assert_response :success
   end
 
-  test "should update status" do
-    put :update, id: @status, status: { content: @status.content, name: @status.name }
+  test "should be redirected when try to update and is not logged in" do
+    put :update, id: @status, status: { content: @status.content}
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end
+
+  test "should update status when logged in" do
+    sign_in users(:michael)
+    put :update, id: @status, status: { content: @status.content}
     assert_redirected_to status_path(assigns(:status))
   end
 
--- test/unit/status_test.rb
+++ test/unit/status_test.rb
@@ -1,7 +1,29 @@
 require 'test_helper'
 
 class StatusTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test "that a status require contents" do
+    status = Status.new
+    assert !status.save
+    assert !status.errors[:content].empty?
+  end
+  
+  test "that a status has at least 2 letters" do
+    status = Status.new
+    status.content = "M"
+    
+    assert !status.save
+    assert !status.errors[:content].empty?
+  end
+
+  test "that a status has a user id" do
+    status = Status.new
+
+    status.content = "Hello"
+    
+    assert !status.save
+    assert !status.errors[:content].empty?
+
+  end
+
 end
